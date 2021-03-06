09f562a9699e9ac31afd522ae8c268036cde55b2
Meera <meera.patel2984@gmail.com>
2021-03-06 14:38:15 +0000

    RChore: move card styling to a separate file

--- components/characterCard.js
+++ components/characterCard.js
@@ -1,3 +1,4 @@
+import {styles} from './characterCardStyle.js';
 
 
 export class CharacterCard extends HTMLElement {
@@ -7,37 +8,7 @@ export class CharacterCard extends HTMLElement {
     this.attachShadow({mode: 'open'});
     this.shadowRoot.innerHTML = `
       <style>
-        * {
-          font-family: sans-serif;
-          text-align: center;
-        }
-        h3 {
-          color: cornflowerblue;
-          margin: 0;
-          padding: 5px 0px 5px 0px;
-        }
-        p {
-          margin: 0;
-          padding: 5px 0px 5px 0px;
-        }
-        .character-card {
-          display: flex;
-          flex-direction: column;
-          width: 210px;
-          height: 370px;
-          padding: 10px;
-          margin: 20px;
-          box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
-
-        }
-        .character-image {
-          margin-bottom: 15px;
-        }
-        .character-details {
-          display:flex;
-          flex-direction: column;
-          align-items: center;
-        }
+        ${styles}
       </style>
     `;
   }
---
+++ components/characterCardStyle.js
@@ -0,0 +1,31 @@
+export const styles = `* {
+  font-family: sans-serif;
+  text-align: center;
+}
+h3 {
+  color: cornflowerblue;
+  margin: 0;
+  padding: 5px 0px 5px 0px;
+}
+p {
+  margin: 0;
+  padding: 5px 0px 5px 0px;
+}
+.character-card {
+  display: flex;
+  flex-direction: column;
+  width: 210px;
+  height: 370px;
+  padding: 10px;
+  margin: 20px;
+  box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
+
+}
+.character-image {
+  margin-bottom: 15px;
+}
+.character-details {
+  display:flex;
+  flex-direction: column;
+  align-items: center;
+}`;
